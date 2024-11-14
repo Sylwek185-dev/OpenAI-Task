93073b69f792f0db82437c293c839ebbd560ecce
Sylwek <sylwek185@op.pl>
2024-11-14 17:49:18 +0100

    form settings

--- static/js/main.js
+++ static/js/main.js
@@ -6,3 +6,37 @@ const showYearOnFooter = () => {
 };
 
 showYearOnFooter();
+
+document
+	.getElementById('contact-form')
+	.addEventListener('submit', function (event) {
+		event.preventDefault(); // Zatrzymanie domyślnego wysłania formularza
+
+		// Przypisanie adresu e-mail z pola do ukrytego pola "_replyto"
+		const email = document.getElementById('email').value;
+		document.getElementById('replyto').value = email;
+
+		const name = document.getElementById('name').value;
+		const message = document.getElementById('message').value;
+
+		// Utworzenie obiektu FormData
+		const formData = new FormData();
+		formData.append('name', name);
+		formData.append('email', email);
+		formData.append('message', message);
+
+		// Wysłanie danych formularza do FormSubmit
+		fetch('https://formsubmit.co/sylwek185@op.pl', {
+			method: 'POST',
+			mode: 'no-cors',
+			body: formData,
+		})
+			.then(() => {
+				alert('Wiadomość została wysłana pomyślnie!');
+				document.getElementById('contact-form').reset();
+			})
+			.catch((error) => {
+				console.error('Błąd:', error);
+				alert('Wystąpił błąd. Spróbuj ponownie później.');
+			});
+	});
